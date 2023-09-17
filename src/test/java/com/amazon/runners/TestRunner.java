2507a5025e1ba8413b632cf7ddb00d794d188568
Merve <safie.merveozer@gmail.com>
2023-09-17 16:08:35 -0500

    Setup complate and Runners Classes created

---
+++ src/test/java/com/amazon/runners/TestRunner.java
@@ -0,0 +1,21 @@
+package com.amazon.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class) //it combines step and features
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json"
+        },
+        features = "src/test/resources/features",
+        glue = "com/amazon/stepDefs",
+        dryRun = false,
+        tags = ""
+)
+public class TestRunner {
+}
