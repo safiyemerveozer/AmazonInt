9e5225d551c5836665b51947d33444efb968f979
Merve <safie.merveozer@gmail.com>
2023-09-17 16:43:18 -0500

    Driver Class is cvreated and configuration created

--- src/test/java/com/amazon/utility/Driver.java
+++ src/test/java/com/amazon/utility/Driver.java
@@ -1,4 +1,34 @@
 package com.amazon.utility;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Driver {
+    private Driver(){} //Singleton
+
+    private static InheritableThreadLocal<WebDriver> driverPool=new InheritableThreadLocal<>();
+    public static WebDriver getDriver(){
+        if(driverPool.get()==null){
+            String browser= System.getProperty("browser") !=null ? System.getProperty("browser"):ConfigReader.getProperty("browser");
+
+            switch (browser){
+                case "chrome":
+                    ChromeOptions options=new ChromeOptions();
+                    options.addArguments("--remote-allow-origins=*");
+                    driverPool.set(new ChromeDriver(options));
+                    break;
+                case "firefox":
+                    driverPool.set(new FirefoxDriver());
+            }
+
+        }
+        return driverPool.get();
+    }
+
+    public static void closeDriver(){
+        driverPool.get().quit();
+        driverPool.remove();
+    }
 }
